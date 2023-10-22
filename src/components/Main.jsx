5a4c2cfd27276a0e326afb8769afd61a8b71f18a
kou <chko22069@g.nihon-u.ac.jp>
2023-10-23 03:30:53 +0900

    reactでコンポーネント!!

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import Gallary from "./Gallery";
+import Welcom from "./Welcom";
+
+export default function Main() {
+  return (
+    <main>
+      <Welcom />
+      <Gallery />
+    </main>
+  );
+}
